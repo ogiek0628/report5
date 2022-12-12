10a3ad3e9df4c5dd360ae5ca18cfb7ca900c6651
Keigo Shinjo <e225755@ie.u-ryukyu.ac.jp>
2022-12-12 22:42:51 +0900

    Second commit

--- src/report5.java
+++ src/report5.java
@@ -2,8 +2,17 @@ public class report5 {
 
     public static void main(String[] args){
 
-        String str = null;
-        System.out.println(str.length());
+       try{
+            String str = null;
+            System.out.println(str.length());
+       
+        }catch(NullPointerException e){
+
+            System.out.println("nullPointExceptionがありました。");
+            System.out.println(e.getMessage());
+
+       }
+
 
     }
 
